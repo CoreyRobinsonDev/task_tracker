dafb1b47cb9b5d0e53855821bb5fd292ec2e8a8f
CoreyRobinsonDev <coreyrob52@gmail.com>
2024-12-16 22:15:28 -0500

    feat: create 'add' command

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
@@ -30,8 +33,60 @@ func runCmd(action string, args...string) {
 
 
 func add(desc string) {
-
+	tasks := getTasks()
+	tasks.Elements = append(tasks.Elements, Task{
+			tasks.NextId(),
+			desc,
+			"todo",
+			time.Now(),
+			time.Now(),
+		})
+	jsonBytes := Unwrap(json.Marshal(tasks))
+	Expect(os.WriteFile("./tasks.json", jsonBytes, 0644))
 }
 
-func getTaskFile() 
+type Task struct {
+	Id uint `json:"id"`
+	Desc string `json:"desc"`
+	Status string `json:"status"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type Tasks struct {
+	Elements []Task `json:"tasks"`
+}
+
+func (self Tasks) NextId() uint {
+	if len(self.Elements) == 0 {
+		return 1
+	} 
+	highestId := self.Elements[0].Id
+
+	for _, task := range self.Elements[1:] {
+		if task.Id > highestId {
+			highestId = task.Id
+		}
+	}	
+
+	return highestId + 1
+}
+
+func getTasks() Tasks {
+	var tasks Tasks
+	bytes, err := os.ReadFile("./tasks.json")
+	if err != nil {
+		fmt.Printf(
+			"%s\n",
+			Italic("task.json not found, creating..."),
+		)
+		Unwrap(os.Create("tasks.json"))
+		jsonBytes := Unwrap(json.Marshal(new(Tasks)))
+		Expect(os.WriteFile("./tasks.json", jsonBytes, 0644))
+		bytes = Unwrap(os.ReadFile("./tasks.json"))
+	}
+
+	Expect(json.Unmarshal(bytes, &tasks))
+	return tasks
+}
 
